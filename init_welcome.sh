d1cc909031d085056854e8c816630342785b01d4
Erlend Nossum <en@scienta.no>
2020-08-24 15:38:15 +0200

    Startup adjustments

--- init_welcome.sh
+++ init_welcome.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # Disable apt-get installation warning about vm being ephemeral
+mkdir -p ~/.cloudshell/
 touch ~/.cloudshell/no-apt-get-warning
 
 echo "Init-welcome script, running as $LOGNAME"
