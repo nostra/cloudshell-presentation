c02f8816dccf0422a417f1fcba52c921b139ab60
Erlend Nossum <nossum@gmail.com>
2020-09-02 10:21:34 +0200

    Smaller script improvement

--- init_welcome.sh
+++ init_welcome.sh
@@ -5,4 +5,8 @@ mkdir -p ~/.cloudshell/
 touch ~/.cloudshell/no-apt-get-warning
 
 echo "Init-welcome script, running as $LOGNAME"
-echo "Consider: chsh -s `which zsh` $LOGNAME"
+# As installation is done asynchronously, we might not have zsh yet
+which zsh > /dev/null && echo "Consider: chsh -s `which zsh` $LOGNAME" || echo "Zsh is not installed yet"
+
+# To install slimzsh for the first time:
+# git clone --recursive https://github.com/changs/slimzsh.git ~/.slimzsh
